3c7618c826a7330d16b16e5e24cc702938ccc948
wesclark20 <wesclark@udel.edu>
2024-03-02 18:59:34 -0500

    Completed pure gpt functions calls for Flashcard/Kahoot

--- openai/gptCall.py
+++ openai/gptCall.py
@@ -22,5 +22,25 @@ def gptCallFlashcards(numOfCards, subject):
     	    {"role": "user", "content": "Generate " + numOfCards + " flash cards to help me study for "+ subject + " and organize the front and back in a JSON format"}
   	  ]
 	)
+	print(completion.choices[0].message)
 
-def gptCallKahoot(subject):
+def gptCallKahoot(numOfQuestions, subject):
+	load_dotenv()
+	API_KEY = os.getenv("OPENAI_API_KEY")
+	if API_KEY is None:
+		print("Error: OPENAI_API_KEY is not set.")
+	else:
+		print("OPENAI_API_KEY:", API_KEY)
+
+	client = OpenAI(api_key = API_KEY)
+	completion = client.chat.completions.create(
+	  model="gpt-4",
+	  messages=[
+	    {"role": "system", "content": "You are a professor, skilled in explaining a vast amount of different school subjects"},
+	    {"role": "user", "content": "Generate "+ numOfQuestions +" four answer multiple choice question about "+ subject +" and organize the four answers in a JSON format with the question, the answer, and the three fake answers"}
+	  ]
+	)
+	print(completion.choices[0].message)
+
+gptCallFlashcards("5", "American History")
+gptCallKahoot("1", "American History")
