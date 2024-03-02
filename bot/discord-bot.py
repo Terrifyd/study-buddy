fc1cb855db049d9095ab308c1df096f46a8ef03a
connorbutsmaller <ianduffy@udel.edu>
2024-03-02 15:41:16 -0500

    added corey messages

--- bot/discord-bot.py
+++ bot/discord-bot.py
@@ -1,6 +1,6 @@
 # bot.py
 import os
-
+import random
 import discord
 from dotenv import load_dotenv
 
@@ -8,7 +8,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
@@ -17,5 +17,29 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, glad you could join the study session!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    corey_titles = [
+        'That\'s Lord Emperor to you!',
+        'May his Lordship be blessed eternally',
+        'Praise be to the Lord Emperor'
+    ]
+
+    if "corey" in message.content.lower():
+        response = random.choice(corey_titles)
+        await message.channel.send(response)
+
+
 
 client.run(TOKEN)
